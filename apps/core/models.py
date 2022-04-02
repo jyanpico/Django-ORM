dab6983bfc0d84dcdcf32567e586836ce0b0321e
jyanpico <picojyan@gmail.com>
2022-04-02 20:42:12 +1000

    progress save

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,13 +1,13 @@
+from doctest import REPORT_CDIFF
 from django.db import models
-
+import requests
+import pygal
 # Create your models here.
 
 class DashboardPanel(models.Model):
-
     github_username = models.CharField(max_length=127)
     repo_name = models.CharField(max_length=127)
-
     panel_type = models.CharField(max_length=127, choices=[
         ("piechart", "Pie-chart of languages used"),
         ("barchart", "Bar-chart of languages used"),
-    ])
+    ])  
--- apps/core/views.py
+++ apps/core/views.py
@@ -3,33 +3,37 @@ from .models import DashboardPanel
 import requests
 import pygal
 
-# Two example views. Change or delete as necessary.
-
-
 def view_panels(request):
     dboard_panels = DashboardPanel.objects.all()
+    response = requests.get('https://api.github.com/users/jyanpico/repos')
+    api_data = response.json()
     context = {
-        "dboard_panels": dboard_panels,
+        "dboard": dboard_panels,
+        "api": api_data
     }
     return render(request, "pages/panels.html", context)
 
 def panel_details(request, panel_id):
     panel = DashboardPanel.objects.get(id=panel_id)
-    chart = pygal.Pie()
 
-    #unsure how to deal with API data here
-    response = requests.get('https://api.github.com/users/kickstartcoding/repos')
-    repos = response.json()
+    url = "https://api.github.com/repos/" + panel.github_username + "/" + panel.repo_name + "/languages"
+    response = requests.get(url)
 
-    for repo_dict in repos:
-        value = repo_dict["size"]
-        label = repo_dict["name"]
-        chart.add(label, value)
-    chart_svg_as_datauri = chart.render_data_uri()
+    # chart = pygal.Pie()
+
+    # for repo_dict in repos:
+    #     value = repo_dict["size"]
+    #     label = repo_dict["name"]
+    #     chart.add(label, value)
+    # chart_svg_as_datauri = chart.render_data_uri()
+    # context = {
+    #     "panel": panel,
+    #     "rendered_chart_svg": chart_svg_as_datauri,
+    # }
+    
+    print('------------------------------------- panel detail testing')
     context = {
         "panel": panel,
-        "rendered_chart_svg": chart_svg_as_datauri,
+        "raw_data": response.json(),
     }
-    return render(request, "pages/panel_details.html", context)
-
-
+    return render(request, "pages/details.html", context)
